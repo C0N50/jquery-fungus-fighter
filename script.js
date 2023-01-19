f667fb45d48638bd873aba1c20f608d05449356b
Connor Ferris <ferr0373@gmail.com>
2023-01-19 17:47:56 -0600

    first commit cuz accidently commiting to color-blocks

--- script.js
+++ script.js
@@ -3,16 +3,83 @@ $(document).ready(onReady);
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+
+let fungusHP = 100;
+let playerAP = 100;
+
+let fungusAlive = true;
+let outOfAP = false;
+
+
+
+
 
 function onReady() {
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
-    
+    $('.attack-btn').on('click', attack);
+
+
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+}
+
+
+function attack () {
+    let attack = $(this).attr('class');
+
+    switch (attack) {
+        case 'attack-btn arcane-sceptre' :
+            playerAP -= 12;
+            fungusHP -= 14;
+            console.log('Player used Arcane Scepter! Freaky Fungus takes 14 Damage!');
+            console.log(`Player AP: ${playerAP}. Freaky Fungus HP: ${fungusHP}`);
+            break;
+        case 'attack-btn entangle' :
+            playerAP -= 23;
+            fungusHP -= 9;
+            console.log('Player used Entangle! Freaky Fungus takes 9 Damage!');
+            console.log(`Player AP: ${playerAP}. Freaky Fungus HP: ${fungusHP}`);
+            break;
+        case 'attack-btn dragon-blade':
+            playerAP -= 38;
+            fungusHP -= 47;
+            console.log('Player used Dragon Blade! Freaky Fungus takes 47 Damage!');
+            console.log(`Player AP: ${playerAP}. Freaky Fungus HP: ${fungusHP}`);
+            break;
+        case 'attack-btn star-fire' :
+            playerAP -= 33;
+            fungusHP -= 25;
+            console.log('Player used star-fire! Freaky Fungus takes 25 Damage!');
+            console.log(`Player AP: ${playerAP}. Freaky Fungus HP: ${fungusHP}`);
+            break;
+    }
+
+    if (playerAP <=0 && fungusHP > 0) {
+        playerAP = 0;
+        outOfAP = true;
+        console.log(`Player Has been defeated! Humanity is Doomed!`);
+    }
+    if (fungusHP <= 0) {
+        fungusHP = 0;
+        fungusAlive = false;
+        console.log(`You have defeated the freaky fungus! You are earth's greatest hero!`);
+    }
+    render();
+}
+
+function render(){
+
+    //render player
+    $('.ap-text').text(playerAP);
+    $('#ap-meter').val(playerAP);
+
+    //render freaky fungus
+    $('.hp-text').text(fungusHP);
+    $('#hp-meter').val(fungusHP);
+    
 }
